7a48c39122896c42717327705dc3a07ad3cc9560
Anderson Ortiz <ortizone620@gmail.com>
2026-02-12 10:11:37 -0600

    S4C2

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+class Inventario {
+    id: number;
+    producto: string;
+    codigo: string;
+    fecha: string;
+    precio: number;
+
+    constructor(id: number, producto: string, codigo: string, fecha: string, precio: number) {
+        this.id = id;
+        this.codigo = codigo;
+        this.producto = producto;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+
+    Agregar(precio2: number, precio3: number): void {
+        console.log(`Producto agregado con precio: $${this.precio}`);
+        console.log(`Precios de descuento: $${precio2} $${precio3}`);
+
+    }
+}
+
+let producto = new Inventario(1, 'Toallas humedas', 'Psdm32', '2026/02/12', 2.5);
+producto.Agregar(2.45, 2.40);
