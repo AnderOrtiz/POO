0d593c86676057b8a4bfb8742a586e302a6734ec
Anderson Ortiz <ortizone620@gmail.com>
2026-02-26 10:05:25 -0600

    S6C2

--- src/Libro.ts
+++
@@ -1,5 +0,0 @@
-/**
-4. Crear una clase Libro con los atributos titulo, autor y numeroPaginas. 
-Instanciar al menos dos objetos y mostrar la información de cada uno 
-utilizando un método.
- */
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,44 @@
-/**
-Crear una clase Estudiante que contenga los siguientes atributos:
-
-nombre (string)
-carnet (string)
-notaFinal (number)
-La clase debe incluir:
-
-Un constructor para inicializar los atributos.
-Un método mostrarInformacion() que imprima los datos del estudiante.
-Posteriormente, instanciar al menos dos objetos y ejecutar el método en cada uno para observar cómo funcionan de manera independiente.
- */
-
-// class Estudiante {
-//     private nombre: string
-//     private readonly carnet: string;
-//     notaFinal: number = 0;
-//     constructor(nombre: string, carnet: string, notaFinal: number) {
-//         this.nombre = nombre;
-//         this.carnet = carnet;
-//         this.notaFinal = notaFinal;
-//     }
-
-//     mostrarInformacion() {
-//         console.log(`Estudiante ${this.nombre}`);
-//         console.log(`Carnet ${this.carnet}`);
-//         console.log(`Nota Final ${this.notaFinal}`);
-
-//     }
-
-//     actualizarNota(nota: number) {
-//         if (nota <= 0 && nota >= 10) {
-//             console.log('Nota no válida');
-
-//         } else {
-//             this.notaFinal = nota;
-//             console.log(`La nota se actalizó a ${this.notaFinal}`);
-//         }
-//     }
-
-// }
-
-// const alumno1 = new Estudiante('Requeno', 'U20200000', 7);
-// alumno1.mostrarInformacion();
-// alumno1.actualizarNota(4)
-
-
-
-/**
-Enunciado:
-
-Crear una clase Producto que incluya:
-
-nombre (string)
-precio (number)
-cantidad (number)
-La clase debe tener:
-
-Un constructor que inicialice los atributos.
-Un método calcularTotal() que retorne el total (precio × cantidad).
-Un método mostrarDetalle() que imprima la información del producto.
-Instanciar al menos un objeto y ejecutar sus métodos.
- */
-
-class Producto {
-    nombre: string
-    precio: number
-    cantidad: number
-
-    constructor(nombre: string, precio: number, cantidad: number) {
+class Animal {
+    constructor(public nombre: string, public categoria: string, public raza: string) {
         this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
+        this.categoria = categoria;
+        this.raza = raza;
     }
 
-    calcularTotal() {
-        const total = this.precio * this.cantidad;
-        console.log(`El total es de $${total}\n`);
+    mostrar() {
+        console.log(`El animal es un ${this.categoria}. Su nombre es ${this.nombre}`);
+    }
+}
+
+class Encargado extends Animal {
+    constructor(nombre: string, categoria: string, raza: string, public dueño: string) {
+        super(nombre, categoria, raza);
+        this.dueño = dueño;
     }
 
-    mostrarDetalle() {
-        console.log(`Nombre: ${this.nombre}`);
-        console.log(`Precio: $${this.precio}`);
-        console.log(`Cantidad: ${this.cantidad}`);
+    mensaje(): void {
+        console.log(`EL animalito es un ${this.categoria} `);
+        console.log(`Se llama: ${this.nombre} su dueño es: ${this.dueño}`);
 
     }
 }
 
-const producto1 = new Producto('Rambo', 12, 2)
-producto1.calcularTotal();
-producto1.mostrarDetalle();
+class Venta extends Encargado {
+    constructor(nombre: string, categoria: string, raza: string, dueño: string, public precio: number) {
+        super(nombre, categoria, raza, dueño)
+        this.precio = precio
+    }
+
+    vendido(): void {
+        console.log(`${this.nombre} se vende a $${this.precio}`);
+
+    }
+}
+
+
+const ob = new Venta('Killer', 'Perro', 'Putbull', 'Miguel', 22);
+ob.mostrar();
+ob.mensaje();
+ob.vendido()
+
+
