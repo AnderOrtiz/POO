7c5ef32a81aff1de02c1d4e09526feb39e22eab2
Anderson Ortiz <ortizone620@gmail.com>
2026-02-27 15:42:44 -0600

    P1

--- src/CuentaBancaria.ts
+++ src/CuentaBancaria.ts
@@ -4,11 +4,29 @@ Implementar métodos para depositar, retirar y mostrar el saldo actual.
  */
 
 class CuentaBancaria {
-    constructor(public depositar: string, public saldo: number) {
-        this.depositar = depositar;
+    constructor(private titular: string, private saldo: number) {
+        this.titular = titular;
         this.saldo = saldo;
     }
 
-    
+    depositar(dinero: number): void {
+        this.saldo += dinero;
+        console.log(`Hi! ${this.titular} Se le notifica que $${dinero} fueron agregado exitosamente`);
+    }
 
+    retirar(dinero: number): void {
+        this.saldo -= dinero;
+        console.log(`Hi! ${this.titular} Se le notifica que $${dinero} fueron retirado exitosamente`);
+    }
+
+    mostrar(): void {
+        console.log(`Hi! ${this.titular} tu saldo actual es de $${this.saldo}`);
+    }
 }
+
+const ander = new CuentaBancaria('Anderson', 100)
+ander.mostrar()
+ander.depositar(50);
+ander.mostrar()
+ander.retirar(75);
+ander.mostrar()
