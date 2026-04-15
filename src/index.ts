da45243908e5e65ff3e0f30cf2b3987cdd7c10f6
Anderson Ortiz <ortizone620@gmail.com>
2026-04-14 22:36:14 -0600

    Lab2

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,115 @@
 import * as readline from "readline";
 
-class Clientes {
+class Paciente {
+    constructor(
+        public tipoConsulta: number,
+        public nombre: string,
+        public codigo: string,
+        public fechaNacimiento: string,
+        public edad: number,
+        public telefono: string,
+        public nombreResponsable: string,
+        public telefonoResponsable: string,
+        public sintomas: string
+    ) { }
 
-    iniciar() {
+    get getInfoBasica(): string {
+        return `Paciente: ${this.nombre} | Edad: ${this.edad} | Tel: ${this.telefono}\nResponsable: ${this.nombreResponsable} | Tel Resp: ${this.telefonoResponsable}`;
+    }
+}
+
+abstract class PreConsulta {
+    abstract realizarCobro(paciente: Paciente): void;
+}
+
+class ConsultaGeneral extends PreConsulta {
+    realizarCobro(paciente: Paciente): void {
+        console.log(paciente.getInfoBasica);
+        console.log(`Síntomas: ${paciente.sintomas}`);
+        console.log(`Costo a cancelar $50`);
+
+        const descuento = parseInt(paciente.codigo.slice(-2)) / 100;
+        const total = 50 * (1 - descuento);
+
+        console.log(`Felicidades le aplicaremos un descuento del ${descuento * 100}%`);
+        console.log(`El total a pagar es de: $${total.toFixed(2)}`);
+    }
+}
+
+class ConsultaEspecialidad extends PreConsulta {
+    realizarCobro(paciente: Paciente): void {
+        console.log(paciente.getInfoBasica);
+        console.log(`Síntomas: ${paciente.sintomas}`);
+        console.log(`Costo a cancelar $90`);
+
+        const descuento = parseInt(paciente.codigo.slice(-2)) / 100;
+        const total = 90 * (1 - descuento) + (90 * 0.03);
+
+        console.log(`Felicidades le aplicaremos un descuento del ${descuento * 100}%`);
+        console.log(`Pero se agrega 3% por uso de máquinas`);
+        console.log(`El total a pagar es de: $${total.toFixed(2)}`);
+    }
+}
+
+class Sistema {
+    tomarInformacion(): void {
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
         });
 
-        rl.question('Ingrese su nombre: ', (nombre) => {
-            rl.question('Ingrese su fecha de nacimiento (dd-mm-yyyy): ', (fechaNacimiento) => {
-                rl.question('Ingrese su estado familiar: ', (estadoFamiliar) => {
-                    rl.question('Ingrese su sueldo: ', (sueldo) => {
+        rl.question(
+            'Bienvenido\nQue tipo de consulta le gustaría hoy\nConsulta general (1)\nConsulta de especialidad (2)\nDigite su respuesta: ',
+            tipoConsulta => {
 
-                        const edad = (2026 - parseInt(fechaNacimiento.slice(6, 10)));
-                        const sueldoNum = parseFloat(sueldo);
+                if (tipoConsulta !== '1' && tipoConsulta !== '2') {
+                    console.log('Ingrese una opción correcta');
+                    rl.close();
+                    return this.tomarInformacion();
+                }
 
-                        this.evaluarCliente(nombre.trim(), fechaNacimiento, estadoFamiliar.toLocaleLowerCase(), sueldoNum, edad);
-                        rl.close();
+                rl.question('Ingrese su nombre: ', nombre => {
+                    rl.question('Ingrese su código de carnet estudiantil: ', codigo => {
+                        rl.question('Ingrese su fecha de nacimiento: ', fechaNacimiento => {
+                            rl.question('Ingrese su edad: ', edad => {
+                                rl.question('Ingrese su teléfono: ', telefono => {
+                                    rl.question('Ingrese su nombre de su responsable: ', nombreResponsable => {
+                                        rl.question('Ingrese el teléfono del responsable : ', telefonoResponsable => {
+                                            rl.question('Ingrese los síntomas : ', sintomas => {
+
+                                                const paciente = new Paciente(
+                                                    parseInt(tipoConsulta),
+                                                    nombre,
+                                                    codigo,
+                                                    fechaNacimiento,
+                                                    parseInt(edad),
+                                                    telefono,
+                                                    nombreResponsable,
+                                                    telefonoResponsable,
+                                                    sintomas
+                                                );
+
+                                                let consulta: PreConsulta;
+
+                                                if (tipoConsulta === '1') {
+                                                    consulta = new ConsultaGeneral();
+                                                } else {
+                                                    consulta = new ConsultaEspecialidad();
+                                                }
+
+                                                consulta.realizarCobro(paciente);
+                                                rl.close();
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
                     });
                 });
-            });
-        });
-    }
-
-    evaluarCliente(nombre: string, fechaNacimiento: string, estadoFamiliar: string, sueldo: number, edad: number) {
-        if (estadoFamiliar == 'casado' &&
-            (edad) >= 22 &&
-            (edad) < 32 &&
-            sueldo > 450 &&
-            sueldo < 600
-        ) {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Se le permite otorgar el prestamo de $20000`
-            );
-
-
-        } else if (estadoFamiliar == 'casado' &&
-            (edad) >= 32 &&
-            sueldo > 601 &&
-            sueldo < 1000
-        ) {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Se le permite otorgar el prestamo de $30000`
-            );
-
-
-        } else if (estadoFamiliar == 'soltero' &&
-            (edad) > 21 &&
-            (edad) < 30 &&
-            sueldo > 400 &&
-            sueldo < 600
-        ) {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Se le permite otorgar el prestamo de $2500`
-            );
-
-
-        } else if (estadoFamiliar == 'soltero' &&
-            (edad) >= 30 &&
-            sueldo > 601 &&
-            sueldo < 1000
-        ) {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Se le permite otorgar el prestamo de $3500`
-            );
-
-
-        } else if (estadoFamiliar == 'viudo' &&
-            (edad) >= 35 &&
-            sueldo > 600 &&
-            sueldo < 1500
-        ) {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Se le permite otorgar el prestamo de $4000`
-            );
-        } else {
-            console.log(
-                `${nombre}, ${estadoFamiliar} de ${edad} años con un sueldo de $${sueldo}\n
-                Lamentamos informarle que no cumple los requisitos para un prestamo`
-            );
-        }
-
+            }
+        );
     }
 }
 
-const cliente = new Clientes()
-cliente.iniciar()
-
-
-
-
-// const cliente1 = new Clientes('Carlos Mendoza', '10-05-2000', 'casado', 500)
-// cliente1.evaluarCliente()
-
-// const cliente2 = new Clientes('Luis Herrera', '15-03-1990', 'casado', 800)
-// cliente2.evaluarCliente()
-
-// const cliente3 = new Clientes('Mario López', '20-07-2002', 'soltero', 500)
-// cliente3.evaluarCliente()
-
-// const cliente4 = new Clientes('Andrés Ramírez', '11-09-1995', 'soltero', 700)
-// cliente4.evaluarCliente()
-
-// const cliente5 = new Clientes('Pedro Castillo', '08-02-1985', 'viudo', 900)
-// cliente5.evaluarCliente()
-
-// const cliente6 = new Clientes('Diego Flores', '01-01-2008', 'soltero', 300)
-// cliente6.evaluarCliente()
+new Sistema().tomarInformacion();
